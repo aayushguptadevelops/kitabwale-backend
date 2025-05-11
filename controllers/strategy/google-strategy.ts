cc7b3fd5048d7eec39ec904ef490e29ff0a3b139
Aayush Gupta <aayushgupta69official@gmail.com>
2025-05-11 21:30:29 +0530

    Implemented User authentication via Google OAuth using passport.js

---
+++ controllers/strategy/google-strategy.ts
@@ -0,0 +1,51 @@
+import passport from "passport";
+import { Strategy as GoogleStrategy, Profile } from "passport-google-oauth20";
+import dotenv from "dotenv";
+import { Request } from "express";
+import User, { IUser } from "../../models/user";
+
+dotenv.config();
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+      callbackURL: process.env.GOOGLE_CALLBACK_URL || "",
+      passReqToCallback: true,
+    },
+    async (
+      req: Request,
+      accessToken,
+      refreshToken,
+      profile,
+      done: (error: any, user?: IUser | false) => void,
+    ) => {
+      const { emails, displayName, photos } = profile;
+      console.log("This is my profile", profile);
+      try {
+        let user = await User.findOne({ email: emails?.[0]?.value });
+        if (user) {
+          if (!user.profilePicture && photos?.[0]?.value) {
+            user.profilePicture = photos?.[0]?.value;
+            await user.save();
+          }
+          return done(null, user);
+        }
+
+        user = await User.create({
+          googleId: profile.id,
+          name: displayName,
+          emails: emails?.[0]?.value,
+          profilePicture: photos?.[0]?.value,
+          isVerified: true,
+          agreeTerms: true,
+        });
+
+        done(null, user);
+      } catch (e) {
+        done(e);
+      }
+    },
+  ),
+);
--- routes/auth-router.ts
+++ routes/auth-router.ts
@@ -1,6 +1,9 @@
-import { Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
 import * as authController from "../controllers/auth-controller";
 import { authenticatedUser } from "../middleware/auth-middleware";
+import passport from "passport";
+import { IUser } from "../models/user";
+import { generateToken } from "../utils/generate-token";
 
 const router = Router();
 
@@ -12,5 +15,31 @@ router.post("/reset-password/:token", authController.resetPassword);
 router.get("/logout", authController.logout);
 
 router.get("/verify-auth", authenticatedUser, authController.checkUserAuth);
+router.get(
+  "/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  }),
+);
+router.get(
+  "/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: `${process.env.FRONTEND_URL}`,
+    session: false,
+  }),
+  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+      const user = req.user as IUser;
+      const accessToken = generateToken(user);
+      res.cookie("access_token", accessToken, {
+        httpOnly: true,
+        maxAge: 24 * 60 * 60 * 1000,
+      });
+      res.redirect(`${process.env.FRONTEND_URL}`);
+    } catch (e) {
+      next(e);
+    }
+  },
+);
 
 export default router;
