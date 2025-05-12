d33c53c154ecd231c19af156c2e3de34b40f0f26
Aayush Gupta <aayushgupta69official@gmail.com>
2025-05-12 20:35:45 +0530

    Created endpoints for razorpay integration

--- routes/order-router.ts
+++ routes/order-router.ts
@@ -7,5 +7,15 @@ const router = Router();
 router.post("/", authenticatedUser, orderController.createOrUpdateOrder);
 router.get("/", authenticatedUser, orderController.getOrderByUser);
 router.get("/:id", authenticatedUser, orderController.getOrderById);
+router.post(
+  "/payment-razorpay",
+  authenticatedUser,
+  orderController.createPaymentWithRazorpay,
+);
+router.post(
+  "/razorpay-webhook",
+  authenticatedUser,
+  orderController.handleRazorpayWebhook,
+);
 
 export default router;
